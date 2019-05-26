1995cedcc4d8c2fdd8397b45bdb8dec4708ed8d7
Mithrakajal <48437851+Mithrakajal@users.noreply.github.com>
2019-05-26 12:00:19 +0530

    Create Alphaornot.java

---
+++ Alphaornot.java
@@ -0,0 +1,10 @@
+public class Alphaornot
+{
+	public static void main(String[] args){
+	    char c='*';
+	    if ((c>='a'&& c<='z')||(c>='A'&& c<='Z'))
+	    System.out.println("alphabet");
+	    else
+	    System.out.println("not an alphabet");
+	}
+	}
