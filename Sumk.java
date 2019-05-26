3d525fdc78ee8a9f166013a30292252077606b81
Mithrakajal <48437851+Mithrakajal@users.noreply.github.com>
2019-05-26 18:31:32 +0530

    Create Sumk.java

---
+++ Sumk.java
@@ -0,0 +1,13 @@
+public class Sumk
+{
+	public static void main(String[] args) {
+		int n=5,k=2,i=1;
+		int A[]={1,2,3,4,5};
+		int sum=0;
+		while(i<=k)
+		    sum=sum+i;
+		    i++;
+		}
+		System.out.println("sum of k is:"+sum);
+}
+}
